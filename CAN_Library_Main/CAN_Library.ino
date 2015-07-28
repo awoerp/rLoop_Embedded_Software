3013819519c484557bab0b72c91e5effad8918ba
awoerp <woerpela@uwplatt.edu>
2015-07-27 19:42:10 -0500

    Initial Commit

---
+++ CAN_Library_Main/CAN_Library.ino
@@ -0,0 +1,20 @@
+/*
+   This is a combination of two CAN bus libraries that will simplify the
+   use of CAN bus on different hardware platforms.
+
+
+
+
+
+
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ CAN_Library_Main/CAN_Library_Main.h
@@ -0,0 +1,26 @@
+/*
+   This is a combination of two CAN bus libraries that will simplify the
+   use of CAN bus on different hardware platforms.
+
+
+
+
+
+
+*/
+
+#ifndef CAN_LIBRARY_MAIN
+#define CAN_LIBRARY_MAIN
+
+#include "FlexCAN_Library-master\FlexCAN.h"
+
+
+
+
+
+
+
+
+
+
+#endif
